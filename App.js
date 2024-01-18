6655b5712f68483a4a7de8efb9cfeb36adcac946
HuyShtorm <heilmehuy34@gmail.com>
2024-01-18 16:34:54 +0700

    Appjs

--- App.js
+++ App.js
@@ -19,3 +19,63 @@ export default function App() {
   );
 }
 
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+// import TrangChu from '../Zalo/View/TrangChu';
+// import TrangTinNhan from'../Zalo/View/TrangTinNhan';
+// import TrangDangKy from '../Zalo/View/TrangDangKy';
+// import User from '../Zalo/View/User';
+// import SettingDetailsChat from'../Zalo/View/SettingDetailsChat';
+// import TrangDangNhap from '../Zalo/View/TrangDangNhap';
+// import TrangDanhBa from '../Zalo/View/TrangDanhBa';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName='TrangChu'>
+//         <Stack.Screen
+//           name="TrangChu"
+//           component={TrangChu}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="TrangTinNhan"
+//           component={TrangTinNhan}
+//           options={{ title: 'TinNhan' }}
+//         />
+//         <Stack.Screen
+//           name="TrangDangKy"
+//           component={TrangDangKy}
+//           options={{ title: 'DangKy' }}
+//         />
+//            <Stack.Screen
+//           name="User"
+//           component={User}
+//           options={{ title: 'User' }}
+//         />
+//              <Stack.Screen
+//           name="SettingDetailsChat"
+//           component={SettingDetailsChat}
+//           options={{ title: 'User' }}
+//         />
+//            <Stack.Screen
+//           name="TrangDangNhap"
+//           component={TrangDangNhap}
+//           options={{ title: 'DangNhap' }}
+//         />
+//          <Stack.Screen
+//           name="TrangDanhBa"
+//           component={TrangDanhBa}
+//           options={{ title: 'DanhBa' }}
+//         />
+
+
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+
+// }
